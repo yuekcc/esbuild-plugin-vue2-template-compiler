6baa8ce4e9b3d9f1e557ea15bbe068619aa631f2
yuekcc <yuekcc@qq.com>
2021-03-22 22:13:00 +0800

    增加 esmodule 转换

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
 import vueTemplateCompiler from 'vue-template-compiler';
+import transformToEs2015 from 'vue-template-es2015-compiler';
 import fs from 'fs';
 
 function toFunction(body) {
   return `function() {${body}}`;
 }
 
-function compile(code) {
+function compile(source) {
   try {
-    const { render, staticRenderFns, errors } = vueTemplateCompiler.compile(code);
+    const { render, staticRenderFns, errors, tips } = vueTemplateCompiler.compile(source);
+    if (Array.isArray(errors) && errors.length > 0) {
+      return { contents: '', errors };
+    }
 
-    const contents = `module.exports = {
+    if (Array.isArray(tips) && tips.length > 0) {
+      tips.forEach((tip) => {
+        console.log('[vue-template-compiler] tips:', tip);
+      });
+    }
+
+    const code = `module.exports = {
     render: ${toFunction(render)},
     staticRenderFns: [
         ${staticRenderFns.map(toFunction).join(',\n')}
     ]
 }`;
+    const contents = transformToEs2015(code).replace(`module.exports = {`, `export default {`);
+
     return { contents, errors };
   } catch (err) {
     return { errors: [err.message] };
